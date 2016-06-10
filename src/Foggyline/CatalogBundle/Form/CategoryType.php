94865e531c4d68e30dd2a0a4da8146150da9b01a
Branko Ajzele <ajzelegmail.com>
2016-06-10 13:56:11 +0200

    FoggylineCatalog

---
+++ src/Foggyline/CatalogBundle/Form/CategoryType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Foggyline\CatalogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('urlKey')
+            ->add('description')
+            ->add('image')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Foggyline\CatalogBundle\Entity\Category'
+        ));
+    }
+}
