b24dd1f4a23e209de8664b7625e6f43bf8310981
Branko Ajzele <ajzelegmail.com>
2016-07-04 17:47:09 +0200

    Stuff added

---
+++ src/Foggyline/PaymentBundle/Controller/CardController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Foggyline\PaymentBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Foggyline\PaymentBundle\Entity\Card;
+use Foggyline\PaymentBundle\Form\CardType;
+
+/**
+ * Card controller.
+ *
+ * @Route("/card")
+ */
+class CardController extends Controller
+{
+    /**
+     * Lists all Card entities.
+     *
+     * @Route("/", name="card_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cards = $em->getRepository('FoggylinePaymentBundle:Card')->findAll();
+
+        return $this->render('FoggylinePaymentBundle:default:card/index.html.twig', array(
+            'cards' => $cards,
+        ));
+    }
+
+    /**
+     * Creates a new Card entity.
+     *
+     * @Route("/new", name="card_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $card = new Card();
+        $form = $this->createForm('Foggyline\PaymentBundle\Form\CardType', $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($card);
+            $em->flush();
+
+            return $this->redirectToRoute('card_show', array('id' => $card->getId()));
+        }
+
+        return $this->render('FoggylinePaymentBundle:default:card/new.html.twig', array(
+            'card' => $card,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Card entity.
+     *
+     * @Route("/{id}", name="card_show")
+     * @Method("GET")
+     */
+    public function showAction(Card $card)
+    {
+        $deleteForm = $this->createDeleteForm($card);
+
+        return $this->render('FoggylinePaymentBundle:default:card/show.html.twig', array(
+            'card' => $card,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Card entity.
+     *
+     * @Route("/{id}/edit", name="card_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Card $card)
+    {
+        $deleteForm = $this->createDeleteForm($card);
+        $editForm = $this->createForm('Foggyline\PaymentBundle\Form\CardType', $card);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($card);
+            $em->flush();
+
+            return $this->redirectToRoute('card_edit', array('id' => $card->getId()));
+        }
+
+        return $this->render('FoggylinePaymentBundle:default:card/edit.html.twig', array(
+            'card' => $card,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Card entity.
+     *
+     * @Route("/{id}", name="card_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Card $card)
+    {
+        $form = $this->createDeleteForm($card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($card);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('card_index');
+    }
+
+    /**
+     * Creates a form to delete a Card entity.
+     *
+     * @param Card $card The Card entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Card $card)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('card_delete', array('id' => $card->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Foggyline/PaymentBundle/Form/CardType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Foggyline\PaymentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CardType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cardType')
+            ->add('cardNumber')
+            ->add('expiryDate', 'date')
+            ->add('securityCode')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Foggyline\PaymentBundle\Entity\Card'
+        ));
+    }
+}
