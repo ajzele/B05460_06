ba501c50f9e12ebdf34f7b25cbe3ca9dddbd6c6a
Branko Ajzele <ajzelegmail.com>
2016-07-29 07:12:50 +0200

    Added store manager controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -102,35 +102,35 @@ class DefaultController extends Controller
         ));
     }
 
-    /**
-     * Creates a new Category entity.
-     *
-     * @Route("/new-category", name="category_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $category = new \Foggyline\CatalogBundle\Entity\Category();
-        $form = $this->createForm('Foggyline\CatalogBundle\Form\CategoryType', $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /* @var $image \Symfony\Component\HttpFoundation\File\UploadedFile */
-            if ($image = $category->getImage()) {
-                $name = $this->get('foggyline_catalog.image_uploader')->upload($image);
-                $category->setImage($name);
-            }
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-
-            return $this->redirectToRoute('category_show', array('id' => $category->getId()));
-        }
-
-        return $this->render('FoggylineCatalogBundle:default:category/new.html.twig', array(
-            'category' => $category,
-            'form' => $form->createView(),
-        ));
-    }
+//    /**
+//     * Creates a new Category entity.
+//     *
+//     * @Route("/new-category", name="category_new")
+//     * @Method({"GET", "POST"})
+//     */
+//    public function newAction(Request $request)
+//    {
+//        $category = new \Foggyline\CatalogBundle\Entity\Category();
+//        $form = $this->createForm('Foggyline\CatalogBundle\Form\CategoryType', $category);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            /* @var $image \Symfony\Component\HttpFoundation\File\UploadedFile */
+//            if ($image = $category->getImage()) {
+//                $name = $this->get('foggyline_catalog.image_uploader')->upload($image);
+//                $category->setImage($name);
+//            }
+//
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($category);
+//            $em->flush();
+//
+//            return $this->redirectToRoute('category_show', array('id' => $category->getId()));
+//        }
+//
+//        return $this->render('FoggylineCatalogBundle:default:category/new.html.twig', array(
+//            'category' => $category,
+//            'form' => $form->createView(),
+//        ));
+//    }
 }
---
+++ src/AppBundle/Controller/StoreManagerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class StoreManagerController extends Controller
+{
+    /**
+     * @Route("/store_manager", name="store_manager")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('AppBundle:default:store_manager.html.twig');
+    }
+}
